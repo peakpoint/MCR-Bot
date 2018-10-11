0436d7e373aa3a8e5748d365a68041e37cb89ad3
Styphix <35349837+AltenGD@users.noreply.github.com>
2018-10-10 22:07:18 -0400

    allowed range thing yna3eioujhbnomrlswm

--- commands/rng.js
+++ commands/rng.js
@@ -1,19 +1,24 @@
 module.exports.run = async(client, message, args) => {
     let rng
 
-    if (args[0]) {
+    if (args.length > 1) {
+        let min = parseInt(args[0]), 
+            max = parseInt(args[1])
+
+        rng = Math.floor(Math.random() * (max - min + 1) ) + min;
+    } else if (args[0]) {
         rng = Math.round(Math.random() * args[0])
     } else {
         rng = Math.round(Math.random().toString())
     }
-    
+
     message.channel.send(rng)
 }
 
 module.exports.help = {
     name: "rng",
-    desc: "Generates a random number | As suggested by L375",
-    usage: "!RNG <max number>",
+    desc: "Generates a random number",
+    usage: "!RNG [min number] <max number>",
     hidden: false,
     mod: false
 }
